fa486be613390b7784ffb4d0c1212624a3b61e0c
Zhibek <jiko633@gmail.com>
2021-03-03 16:52:12 +0400

    my solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+   if (Array.isArray(array) && array.length ) {
+       return Math.min(...array);
+   } else {
+       return 0;
+   }
 }
 
 exports.max = function max (array) {
-  return 0;
+ if (Array.isArray(array) && array.length) {
+     return Math.max(...array);
+ } else {
+     return 0;
+ }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+   if (Array.isArray(array) && array.length) {
+       let sum = array.reduce((acc, rec) => {
+           return acc + rec;
+       }, 0);
+       let len = array.length;
+       return sum / len;
+   } else {
+       return 0;
+   }
 }
